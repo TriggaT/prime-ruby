ff04378e1781a3cb0676c1f6cbf69664e35f86c5
Trayshawn <traytwebb14@yahoo.com>
2020-11-13 23:29:21 -0600

    commit tree f4735267f66c756bf9b8b1721af989a0528981a9

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+
+
+def prime?(num)
+    if num <= 1
+        return false
+    elsif n = 2
+        while n < num
+            return false if num % n == 0
+            n += 1
+        end
+        true
+    end
+end
+
